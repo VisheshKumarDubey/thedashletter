323c6f5253f54f85c73b64eb8c47cec1af88e406
Vishesh <visheshdubey2016@gmail.com>
2020-04-17 19:24:37 +0530

    chips connected

--- scripts/home.js
+++ scripts/home.js
@@ -1,69 +1,138 @@
+const taggy = document.getElementById('tagg')
 const app = document.getElementById('root')
+const tagu = document.createElement('div')
+tagu.setAttribute('class', 'tags')
+taggy.appendChild(tagu)
+
+
 let data = ""
-    //https://dashletter-backend.herokuapp.com/home
-const section = document.createElement('section');
+let tagData;
+let sp = 0;
+let ep = 50;
+let current_url = "https://dashletter-backend.herokuapp.com/blog/?category=all";
+let section = document.createElement('section');
 section.setAttribute('class', 'section');
-const container = document.createElement('div')
+let container = document.createElement('div')
 container.setAttribute('class', 'container is-desktop')
-let movie;
-var sp =0;
-var ep =100;
-//app.appendChild(logo)
+
+async function xyz() {
+
+    const tagni = await axios.get('https://dashletter-backend.herokuapp.com/blog/?category=allcategory')
+        .then((response) => {
+            return response.data;
+        }
+            , (error) => {
+                console.log(error);
+
+            });
+    return tagni;
+}
+
+//Tag based Loading
+
+
+(async () => {
+    let taglist = await xyz()
+    let movie;
+    for (movie in taglist) {
+        const tagme = document.createElement('span')
+        tagme.setAttribute('class', "tag is-rounded")
+        tagme.setAttribute('id', "0t" + movie)
+        tagme.textContent = taglist[movie].name
+        tagu.appendChild(tagme)
+
+    }
+    document.querySelectorAll('.tag').forEach(item => {
+        item.addEventListener('click', event => {
+            //handle click
+            var c  = "";
+            c=item.id;
+            c=c.split("t")[1];
+            sp = 0; ep = 50;
+            current_url = taglist[c].url;
+            loadmore(current_url, 0, sp, ep)
+        })
+    })
+}
+
+
+)()
+
+//LoadMore Button function
+
 var loadMore = document.querySelector('#loadMore');
-loadMore.addEventListener('click', function(){
-   loadmore();
-  });
-function loadmore()
-{
+loadMore.addEventListener('click', function () {
+    loadmore(current_url, 1, sp, ep);
+});
+
+//Utility Function
+
+const quoteRemover = (text) => {
+    text = text.replace('\"', '')
+    text = text.replace('\"', '')
+    return text;
+}
+
+
+
+//Content Loading
+
+function loadmore(url, g, s, e) {
+    if (g == 0) {
+        section.removeChild(container)
+        container = document.createElement('div')
+        container.setAttribute('class', 'container is-desktop')
+        section.appendChild(container);
+    }
     app.appendChild(section);
     section.appendChild(container)
-    axios.get('https://dashletter-backend.herokuapp.com/blog/?category=fashion&sp='+sp+'&ep='+ep)
+    axios.get(url + '&sp=' + s + '&ep=' + e)
         .then((response) => {
             data = response.data;
+            let movie;
             for (movie in data) {
-                console.log(quoteRemover(JSON.stringify(data[movie].urlToImage)))
                 const a = document.createElement('a')
+                
                 a.setAttribute('href', quoteRemover(JSON.stringify(data[movie].url)))
                 a.setAttribute('target', "_blank")
-    
+
                 const card = document.createElement('div')
                 card.setAttribute('class', 'card')
-    
+
                 const card_image = document.createElement('div')
                 card_image.setAttribute('class', 'card-image')
-    
+
                 const figure = document.createElement('figure')
                 figure.setAttribute('class', 'image is-4by3')
-    
+
                 const img = document.createElement('img')
-    
+
                 let image = quoteRemover(JSON.stringify(data[movie].urlToImage))
                 img.setAttribute('src', image)
-                console.log(JSON.stringify(data[movie].urlToImage))
-    
+
                 const card_content = document.createElement('div')
                 card_content.setAttribute('class', 'card-content')
-    
+
                 const media = document.createElement('div')
                 media.setAttribute('class', 'media')
-    
+
                 const media_content = document.createElement('div')
                 media_content.setAttribute('class', 'media-content')
-    
+
                 const p_title = document.createElement('p')
                 p_title.setAttribute('class', 'titly title is-5')
                 data[movie].title = quoteRemover(JSON.stringify(data[movie].title))
                 p_title.textContent = data[movie].title
-    
+
                 const p_subtitle = document.createElement('p')
                 p_subtitle.setAttribute('class', 'titly subtitle is-7')
                 p_subtitle.textContent = `source: ${quoteRemover(JSON.stringify(data[movie].source.name))}`
-    
+
                 const div_content = document.createElement('div')
                 div_content.setAttribute('class', 'content')
                 data[movie].content = quoteRemover(JSON.stringify(data[movie].content).substring(0, 300))
                 div_content.textContent = `${data[movie].content}<a>read more...</a>`
-    
+
                 container.appendChild(a)
                 a.appendChild(card)
                 card.appendChild(card_image)
@@ -75,18 +144,17 @@ function loadmore()
                 media_content.appendChild(p_title)
                 media_content.appendChild(p_subtitle)
                 card_content.appendChild(div_content)
-    
+
             }
+            sp = sp+ 50;
+            ep = ep+50;
         }, (error) => {
             console.log(error);
-    
+
         });
-        sp=sp+50;
-        ep =ep+50;
+    
 }
-loadmore();
-const quoteRemover = (text) => {
-    text = text.replace('\"', '')
-    text = text.replace('\"', '')
-    return text;
-}
+
+
+//Initial content loading
+loadmore(current_url, 1, sp, ep);
--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,6 @@
- //var axios = require('axios')
+import {login, signup, logout} from './server'
 let data;
-axios.post("http://dashletter-backend.herokuapp.com/login"
-
-, {
-   email: 'satya',
-   password: 'satya'
-}
-)
-   .then((response) => {
-      
-    console.log(response)
-    
-    console.log(response.status)
-    console.log("asdfghjklkjhgfdsasdfghjklkjhgfdsasdfghjkl")
-   //document.write(JSON.stringify(response))
-   }
-
-   )
-   .catch((err) => console.log("Something went wrong!", err));
+login('satya', 'satya');
+// console.log(categFetch('fashion',0 ,5))
 
 
-// // Store data
-// var someTempData = 'hello osama';
-// sessionStorage.setItem('myTempDataKey', someTempData);
-
-// Get data
-//var tempData = sessionStorage.getItem('myTempDataKey');
-//console.log(tempData)
-// Remove data
-//sessionStorage.removeItem('myTempDatakey');
-
-
-
-//console.log(data)
